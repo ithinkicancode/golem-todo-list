cf3cdc86949b86c75708063c356cf4fdd97aa000
Terry Lin <tlin.gta@gmail.com>
2023-08-04 12:07:26 -0400

    cleanup

--- lib/src/core.rs
+++ lib/src/core.rs
@@ -23,7 +23,7 @@ pub fn u64_from(
 ) -> AppResult<u64> {
     u64::try_from(n).map_err(
         |e| {
-            format!("ERROR converting {} to u64: {}", n, e.to_string())
+            format!("ERROR converting {} to u64: {:?}", n, e.to_string())
         }
     )
 }
